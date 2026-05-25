d546c27c95cde26e8308efce883d7c740c1711c7
qaisar-data-science <qaisar.islam.537@gmail.com>
2026-05-25 11:45:54 +0400

    ready

--- tests/testCases.py
+++ tests/testCases.py
@@ -1,7 +1,8 @@
 from playwright.sync_api import Page
 
 
-def testLogin(page: Page):
+def test_login(page: Page):
     page.goto("https://simple-login-cicd.vercel.app/")
     assert "Login" in page.title
+    print("Login page found test has passes!")
     
