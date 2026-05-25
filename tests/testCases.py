8045680151a65143241e0f9afeefdc91b5415d6e
qaisar-data-science <qaisar.islam.537@gmail.com>
2026-05-25 11:36:05 +0400

    added cicd actions

--- tests/testCases.py
+++ tests/testCases.py
@@ -2,6 +2,6 @@ from playwright.sync_api import Page
 
 
 def testLogin(page: Page):
-    page.goto("https://kjsadhk.com")
+    page.goto("https://vercel.com")
     assert "Login" in page.title
     
