ca4a9cf3094ce320cd0804c57dac5fded1e2c543
Kaskar333 <53970361+Kaskar333@users.noreply.github.com>
2026-06-17 13:53:06 +0400

    Update test_userApi.py

--- tests/test_userApi.py
+++ tests/test_userApi.py
@@ -2,26 +2,11 @@ import requests
 import time
 
 getUrl = "https://6a1bf7688858a003817b5dee.mockapi.io/testautomation/userCollection"
-postUrl = "https://6a1bf7688858a003817b5dee.mockapi.io/testautomation/userCollection"
-payload = {
-    "name": "Pytest Mertz",
-    "email": "pytest@hotmail.com",
-    "phone": "(453) 703-0753 x0538",
-    "address": "Sandhurst"
-  }
 
 def test_getUser():
     response = requests.get(getUrl)
     assert response.status_code == 200
     print(response.json())
-
-def test_postUser():
-    response = requests.post(postUrl, data=payload)
-    assert response.status_code == 201
-    data = response.json()
-    if data["name"] == payload["name"]:
-        print("The test was a success!")
-
-test_postUser()
-time.sleep(5)
+    
 test_getUser()
+
