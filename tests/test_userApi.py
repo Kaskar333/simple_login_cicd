f76b9f4c97c1fb39797ddfa8eb479efc1bf6a0ff
Kaskar333 <53970361+Kaskar333@users.noreply.github.com>
2026-06-17 13:55:27 +0400

    Update test_userApi.py

--- tests/test_userApi.py
+++ tests/test_userApi.py
@@ -1,12 +1,10 @@
 import requests
 import time
 
-getUrl = "https://6a1bf7688858a003817b5dee.mockapi.io/testautomation/userCollection"
+getUrl = "https://6a1bf7688858a003817b5dee.mockapi.io/orderdata/userCollection"
 
 def test_getUser():
     response = requests.get(getUrl)
-    assert response.status_code == 200
-    print(response.json())
     
 test_getUser()
 
